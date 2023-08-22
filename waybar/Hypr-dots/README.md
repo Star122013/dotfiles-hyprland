ac1190395061299ebeddf9a4c5d8f73d6380bb5f
He Yanyu <137638765+Star-Language@users.noreply.github.com>
2023-08-23 02:34:23 +0800

    Add files via upload
    初始化

---
+++ waybar/Hypr-dots/README.md
@@ -0,0 +1,46 @@
+# Hypr Dotfiles
+
+
+This is a customized dotfiles collection with catppuccin mocha palette. 
+
+## Screenshots
+![](screenshots/1.png)
+
+![](screenshots/2.png)
+
+## Installation
+
+```git clone https://github.com/tempestian/Hypr-dots.git```
+
+```cd Hypr-dots```
+
+```./install.sh```
+
+## Keybinds
+
+Mod = SUPER
+
+| Task | Keybind | 
+| ------------- |:-------------:|
+| Terminal | Mod + Return |
+| App Menu | Mod + D |
+| Window Kill | Mod + Q |
+| Screenshot | PRINT |
+| Floating | Mod + F |
+| Full Screen | Alt + F |
+
+-------------------------------------------------------------------------------------------------------------
+You can fork this dotfile to your liking. 
+
+If you encounter any bugs or have any suggestions, feel free to write.
+
+
+### Thanks <3
+
+https://github.com/catppuccin/catppuccin
+
+https://github.com/vinceliuice/Tela-circle-icon-theme
+
+https://github.com/tw0xa
+
+https://github.com/m3tozz/NeoCat
---
+++ waybar/Hypr-dots/install.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+# Hypr-dots installer script for Arch Linux.
+# Do NOT run this script as root...
+
+unset -v sucmd
+
+while true; do
+    read -p "Enter the privilege escalation command (sudo, doas...): " sucmd
+    if which "$sucmd" > /dev/null 2>&1; then
+        echo "Command exists! Continuing with $sucmd."
+        break
+    else
+        echo "Command does not exist. Try again."
+    fi
+done
+
+echo "DO NOT RUN THIS SCRIPT AS ROOT!!!"
+echo "This script will install necessary packages and overwrite any file that conflicts with Hypr-dots config files. You will NOT prompted for any change."
+echo "Make sure to run this at Hypr-dots folder."
+echo "Are you sure you want to continue? (Press any key to continue or ctrl + c to exit without making ANY change...)"
+read
+echo "Starting installation..."
+tmp=/tmp/hypr-temp
+mkdir $tmp
+cp -r etc $tmp
+cp -r config $tmp
+cd
+echo "Upgrading system..."
+$sucmd pacman -Syu
+echo "Checking if paru is installed..."
+if [[ $(which paru) ]]; then
+    echo "Paru is already installed."
+else
+    echo "Paru not found. Installing paru..."
+    $sucmd pacman -Syy git fakeroot go make gcc --noconfirm --needed
+    git clone https://aur.archlinux.org/paru-bin.git /tmp/paru-bin/
+    cd /tmp/paru-bin/
+    makepkg -si
+    echo "Paru installed!"
+fi 
+
+echo "Installing dependencies..."
+$sucmd pacman -S --needed hyprland waybar xdg-desktop-portal-hyprland polkit-kde-agent btop pamixer pavucontrol fish kitty starship noto-fonts dolphin mako wofi qt5ct kvantum lxappearance pkgconf which neofetch --noconfirm
+paru -S --needed swww wlogout catppuccin-gtk-theme-mocha sddm-catppuccin-git catppuccin-cursors-mocha hyprshot ttf-jetbrains-mono-nerd kvantum-theme-catppuccin-git
+
+echo "Enabling SDDM..."
+$sucmd systemctl enable sddm
+
+echo "Setting Kitty terminal theme..."
+kitty +kitten themes --reload-in=all Catppuccin-Mocha
+
+echo "Set fish as shell"
+chsh -s $(which fish)
+
+echo "Installing icons..."
+cd /tmp
+git clone https://github.com/vinceliuice/Tela-circle-icon-theme
+cd Tela-circle-icon-theme
+chmod +x install.sh
+bash install.sh -c purple
+cd
+
+echo "Default app settings..."
+xdg-mime default vim.desktop text/plain
+xdg-mime default org.kde.dolphin.desktop inode/directory
+xdg-mime default firefox.desktop x-scheme-handler/http
+xdg-mime default firefox.desktop x-scheme-handler/https
+kwriteconfig5 --file ~/.local/share/kservices5/ServiceMenus/konsolehere.desktop --group "Desktop Action OpenTerminal" --key "Name" "Open in Kitty Terminal"
+kwriteconfig5 --file ~/.local/share/kservices5/ServiceMenus/konsolehere.desktop --group "Desktop Action OpenTerminal" --key "Exec" "kitty"
+
+echo "Copying files..."
+$sucmd cp $tmp/etc/environment /etc
+$sucmd cp $tmp/etc/sddm.conf /etc
+mkdir ~/.config bb&> /dev/null
+cp -r $tmp/config/* ~/.config
+
+echo "Installation finished!"
